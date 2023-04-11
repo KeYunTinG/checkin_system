38cd4981a2baca38a710c0183470e861c6cc05c3
zaac2240632 <zaac2240632@yahoo.com.tw>
2023-04-11 12:49:34 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const helpers = require('./_helpers');
+const session = require('express-session');
+//const passport = require('./config/passport')
+const handlebars = require('express-handlebars')
+const routes = require('./routes')
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const app = express()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const port = process.env.PORT || 3000
+
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(methodOverride('_method'));
+app.use(express.static('public'))
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
+app.use(express.urlencoded({ extended: true }))
+//app.use(passport.initialize())
+//app.use(passport.session())
+app.use(flash())
+// app.use((req, res, next) => {
+//   res.locals.user = helpers.getUser(req);
+//   res.locals.isAuthenticated = helpers.ensureAuthenticated(req);
+//   res.locals.successMessage = req.flash('successMessage');
+//   res.locals.errorMessage = req.flash('errorMessage');
+//   res.locals.successFlashMessage = req.flash('successFlashMessage');
+//   res.locals.errorFlashMessage = req.flash('errorFlashMessage');
+//   next();
+// });
+app.use(routes)
+
+app.listen(port, () => console.log(`Example app listening on port ${3000}!`))
+
+module.exports = app
---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.Router()
+//const userController = require('../controllers/user-controller')
+//const adminController = require('../controllers/admin-controller')
+//const passport = require('../config/passport')
+//const helpers = require('../_helpers')
+//const { generalErrorHandler } = require('../middleware/error-handler')
+//const { authenticated, adminAuthenticated } = require('../middleware/auth')
+
+
+// router.get('/signup', userController.registerPage)
+// router.post('/signup', userController.signup)
+
+// router.get('/signin', userController.loginPage)
+// router.post('/signin', passport.authenticate('local', { successRedirect: '/tweets', failureRedirect: '/signin' }))
+// router.get('/logout', userController.signout)
+
+
+
+router.get('/', (req, res) => res.render('login'))
+//router.use('/', generalErrorHandler)
+
+module.exports = router
