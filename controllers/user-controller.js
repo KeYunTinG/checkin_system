935398938c8f35ec3f6a9dfdb59069c389a450d7
KeYunTinG <zaac2240632@yahoo.com.tw>
2023-04-23 17:04:18 +0800

    edit password

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,18 +1,39 @@
-
-
+const bcrypt = require('bcryptjs')
+const db = require('../models');
+const User = db.User;
 const userController = {
 
     signInPage: (req, res) => {
-        res.redirect('/signin')
+        res.render('login')
     },
     signIn: (req, res) => {
         req.flash('successMessage', '成功登入!')
         res.redirect('/home');
     },
     signout: (req, res) => {
-        req.flash('successMessage', '登出成功！');
-        req.logout();
-        res.redirect('/login');
+        req.flash('successMessage', '登出成功！')
+        req.logout()
+        res.redirect('/signin')
+    },
+    settingPage: (req, res) => {
+        res.render('setting')
+    },
+    setting: (req, res) => {
+        const password = req.body.password
+        const checkPassword = req.body.checkPassword
+        if (password === checkPassword) {
+            return bcrypt.genSalt(10).then(salt => {
+                return Promise.all([bcrypt.hash(password, salt), User.findByPk(req.user.id)])
+            }).then(([hash, user]) => {
+                return user.update({ password: hash })
+            }).then(() => {
+                req.flash('successMessage', '修改密碼成功')
+                res.redirect('back')
+            })
+        } else {
+            req.flash('errorMessage', '兩次密碼輸入不符')
+            res.redirect('back')
+        }
     },
 }
 
--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,9 @@ router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/home', authenticated, checkinController.homepage)
 router.post('/checkin', authenticated, checkinController.checkin)
-// router.get('/logout', userController.signout)
+router.get('/setting', authenticated, userController.settingPage)
+router.post('/setting', authenticated, userController.setting)
+router.get('/logout', userController.signout)
 
 
 router.get('/', (req, res) => res.render('login'))
